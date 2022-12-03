59fb7d4d0f6dec526ccca46552c7747a971581da
Daniel Siemssen <daniel@dr-siemssen.de>
2022-12-03 06:54:36 +0100

    Day 03

---
+++ day03.py
@@ -0,0 +1,11 @@
+abc = [*" abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+three = {*abc}
+part1 = part2 = 0
+for i, items in enumerate(open(0)):
+    half = len(items) // 2
+    part1 += abc.index(({*items[:half]} & {*items[half:]}).pop())
+    three &= {*items}
+    if i % 3 == 2:
+        part2 += abc.index(three.pop())
+        three = {*abc}
+print(part1, part2)
