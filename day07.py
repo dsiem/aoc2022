4977c8d99498ef73ea0475abb8d4996b21f6729e
Daniel Siemssen <daniel@dr-siemssen.de>
2022-12-07 07:30:21 +0100

    Day 07

---
+++ day07.py
@@ -0,0 +1,18 @@
+sizes = {"": 0}
+for line in open(0):
+    match line.split():
+        case ["$", "cd", "/"]:
+            path = []
+        case ["$", "cd", ".."]:
+            path.pop()
+        case ["$", "cd", dir]:
+            path.append(dir)
+            sizes["/".join(path)] = 0
+        case ["$" | "dir", _]:
+            pass
+        case [size, _]:
+            for n in range(len(path)+1):
+                sizes["/".join(path[:n])] += int(size)
+needed = sizes[""] - 40000000
+print(sum(s for s in sizes.values() if s <= 100000), min(s for s in sizes.values() if s >= needed))
+# 1315285 9847279
