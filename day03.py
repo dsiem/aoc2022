36cdf7370d1bf6c92fbafa31a2cf4b1f9007d720
Daniel Siemssen <daniel@dr-siemssen.de>
2022-12-19 12:46:47 +0100

    Cleanup

--- day03.py
+++ day03.py
@@ -1,11 +1,13 @@
-abc = [*" abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"]
-three = {*abc}
+abcde = [*" abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+three = {*abcde}
 part1 = part2 = 0
+
 for i, items in enumerate(open(0)):
     half = len(items) // 2
-    part1 += abc.index(({*items[:half]} & {*items[half:]}).pop())
+    part1 += abcde.index(({*items[:half]} & {*items[half:]}).pop())
     three &= {*items}
     if i % 3 == 2:
-        part2 += abc.index(three.pop())
-        three = {*abc}
+        part2 += abcde.index(three.pop())
+        three = {*abcde}
+
 print(part1, part2)  # 7716 2956
--- day05.py
+++ day05.py
@@ -1,8 +1,10 @@
 lines = open(0).readlines()
-stacks = [*("".join(x).strip() for x in zip(*(f" {crates[1::4]:<9}" for crates in lines[:8])))]
-both = (stacks, stacks[:])
+stack = [*("".join(x).strip() for x in zip(*(f" {crates[1::4]:<9}" for crates in lines[:8])))]
+parts = (stack, stack[:])
+
 for moves in lines[10:]:
     n, src, dst = map(int, moves.split()[1::2])
-    for stacks, mode in zip(both, (-1, 1)):
-        stacks[src], stacks[dst] = stacks[src][n:], stacks[src][:n][::mode] + stacks[dst]
-print(*("".join(stack[0] for stack in stacks[1:]) for stacks in both))  # CVCWCRTVQ CNSCZWLVT
+    for stack, mode in zip(parts, (-1, 1)):
+        stack[src], stack[dst] = stack[src][n:], stack[src][:n][::mode] + stack[dst]
+
+print(*("".join(s[0] for s in stack[1:]) for stack in parts))  # CVCWCRTVQ CNSCZWLVT
