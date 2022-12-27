af39c394d9810bf3c95c01b21f5a1803919e3f22
Daniel Siemssen <daniel@dr-siemssen.de>
2022-12-27 13:39:31 +0100

    Day 12

--- day12.py
+++ day12.py
@@ -1,23 +1,22 @@
 import collections
 
 grid = open(0).read()
-width, peak = grid.find("\n") + 1, grid.find("E")
-deque, seen = collections.deque([(peak, 0)]), {peak}
-final = ["S", "a"]
-parts = []
+width = grid.find("\n") + 1
 
 def allowed(old, new):
     return ord(grid[old]) - ord(grid[new].replace("S", "a")) <= 1
 
-while deque:
-    old, dist = deque.popleft()
-    if grid[old] in final:
-        final.remove(grid[old])
-        parts.insert(0, dist)
-        if not final: break
-    for new in (old + 1, old - 1, old + width, old - width):
-        if new not in seen and 0 <= new < len(grid) and -new % width != 1 and allowed(old, new):
-            seen.add(new)
-            deque.append((new, dist + 1))
+def find_shortest(start_pos, destinations):
+    deque, seen = collections.deque([(start_pos, 0)]), {start_pos}
+    while deque and destinations:
+        old, dist = deque.popleft()
+        if (dest := grid[old]) in destinations:
+            destinations.remove(dest)
+            yield dest, dist
+        for new in (old + 1, old - 1, old + width, old - width):
+            if new not in seen and 0 <= new < len(grid) and -new % width != 1 and allowed(old, new):
+                seen.add(new)
+                deque.append((new, dist + 1))
 
-print(*parts)  # 472 465
+shortest = dict(find_shortest(grid.find("E"), ["S", "a"]))
+print(shortest["S"], shortest["a"])  # 472 465
